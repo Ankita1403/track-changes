9a0f74817b0d8f82e0bb16e45227ce90d638e46f
ankita <ankitapk22@gmail.com>
2019-03-03 12:13:19 +0530

    added test files

---
+++ calculator.js
@@ -0,0 +1,5 @@
+var first = prompt("enter first number");
+var second = prompt("enter second number");
+var sum = Number(first) + Number(second);
+alert("sum of numbers=" + sum);
+
---
+++ js2.js
@@ -0,0 +1,2 @@
+prompt("are you happy?");
+alert("welcome to your second script");
